2a636431cee50785c64cf6738c85c6b23499333e
mahnoor977 <ahsanmahnoor081@gmail.com>
2026-08-26 12:21:25 +0500

    sidebar error detection

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,10 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,400;0,500;1,400;1,500&family=IBM+Plex+Sans:wght@400;500;600&family=IBM+Plex+Mono:wght@400;500&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css"
+        />
       </Head>
       <Component {...pageProps} />
     </>
