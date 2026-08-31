2f0c7147584eb19b87de75bbf564230d8f88e535
mahnoor977 <ahsanmahnoor081@gmail.com>
2026-08-31 12:56:38 +0500

    Add site-wide card entrance animations and smooth page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [transitioning, setTransitioning] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setTransitioning(true);
+    const handleComplete = () => setTransitioning(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
   return (
     <>
       <Head>
@@ -16,7 +36,9 @@ export default function App({ Component, pageProps }) {
           href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css"
         />
       </Head>
-      <Component {...pageProps} />
+        <div className={transitioning ? 'page-transitioning' : ''}>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
